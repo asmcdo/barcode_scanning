2d2c470670da7d2e875cd2c18a99a4eda1e5fab5
Andrei Macedo <macedoandrei@hotmail.com>
2020-07-26 21:58:23 -0300

    initial commit setting up cameraX

---
+++ app/src/main/java/com/example/barcodescanning/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.barcodescanning
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_barcode_scanner.*
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (allPermissionsGranted()) {
+            startListeners()
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (!allPermissionsGranted()) {
+                Toast.makeText(this, R.string.permission_not_granted, Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+    private fun startListeners() {
+        barcodeScannerButton.setOnClickListener {
+            openCameraActivity()
+        }
+    }
+
+    private fun openCameraActivity() {
+        //TODO: open BarcodeScanneractivity
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    companion object {
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSIONS = 10
+    }
+}
---
+++ app/src/main/java/com/example/barcodescanning/barcodeScanner/BarcodeScannerActivity.kt
@@ -0,0 +1,60 @@
+package com.example.barcodescanning.barcodeScanner
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.Camera
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import com.example.barcodescanning.R
+import kotlinx.android.synthetic.main.activity_barcode_scanner.*
+import java.util.concurrent.ExecutorService
+
+class BarcodeScannerActivity(var onBarcodeResult: (a: String) -> Unit) : AppCompatActivity() {
+    private var preview: Preview? = null
+    private var imageAnalyzer: ImageAnalysis? = null
+    private var camera: Camera? = null
+
+    private lateinit var cameraExecutor: ExecutorService
+
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_barcode_scanner)
+        startCamera()
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            preview = Preview.Builder().build()
+
+            val cameraSelector =
+                CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
+
+            try {
+                cameraProvider.unbindAll()
+
+                camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+
+                preview?.setSurfaceProvider(scannerPreview.createSurfaceProvider())
+
+
+            } catch (ex: Exception) {
+                Log.e(TAG, "UseCase binding failed", ex)
+            }
+
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    companion object {
+        private const val TAG = "CameraX"
+    }
+}
